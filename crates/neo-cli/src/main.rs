1c1ab8fb8a35bb31fb0cb4be4746130b4b5a5650
Jeremy Bolding <cyberchefjay@gmail.com>
2026-05-04 22:42:56 -0500

    added: Some Command stubs for `init`,`build`,`server`

--- crates/neo-cli/src/main.rs
+++ crates/neo-cli/src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Command, arg};
 
 fn cli() -> Command {
-    Command::new("neo-cli")
+    Command::new("neo")
         .about("A command-line interface for NeoStackRS Static Site Generator")
         .version(env!("CARGO_PKG_VERSION"))
         .arg_required_else_help(true)
@@ -9,8 +9,24 @@ fn cli() -> Command {
         .subcommand(
             Command::new("new")
                 .about("Create a new NeoStackRS project")
+                .alias("create").alias("n")
                 .arg(arg!(<INPUT> "The input directory containing source files").required(true))
                 .arg(arg!(--template -t [TEMPLATE] "The template to use for the new project").required(false))
+        )
+        .subcommand(
+            Command::new("init")
+                .about("Initialize a new NeoStackRS project")
+                .alias("i")
+        )
+        .subcommand(
+            Command::new("build")
+                .about("Build the static site from the source files")
+                .alias("b")
+        )
+        .subcommand(
+            Command::new("server")
+                .about("Start a development server for the static site")
+                .alias("serve").alias("s")
         )
 }
 
@@ -20,6 +36,15 @@ pub fn main() {
     match matches.subcommand() {
         Some(("new", _sub_matches)) => {
             println!("Creating a new NeoStackRS project...");
+        }
+        Some(("init", _sub_matches)) => {
+            println!("Initializing a new NeoStackRS project...");
+        }
+        Some(("build", _sub_matches)) => {
+            println!("Building the static site...");
+        }
+        Some(("server", _sub_matches)) => {
+            println!("Starting the development server...");
         }
         _ => unreachable!(), // clap will ensure we don't get here
     }
